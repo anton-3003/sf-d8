8fe47d3988f035cb6507a59ffc0a67ab74ffcc21
anton-3003 <68605875+anton-3003@users.noreply.github.com>
2021-03-11 15:23:46 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -68,29 +68,76 @@ LANGUAGE_CODE = 'ru-RU'
 django_heroku.settings(locals())
 
 
+# def get_cache():
+#     environment_ready = all(
+#         os.environ.get(f'MEMCACHIER_{key}', False)
+#         for key in ['SERVERS', 'USERNAME', 'PASSWORD']
+#     )
+#     if not environment_ready:
+#         cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
+#     else:
+#         servers = os.environ['MEMCACHIER_SERVERS']
+#         username = os.environ['MEMCACHIER_USERNAME']
+#         password = os.environ['MEMCACHIER_PASSWORD']
+#         cache = {
+#             'default': {
+#                 'BACKEND': 'django_bmemcached.memcached.BMemcached',
+#                 'TIMEOUT': None,
+#                 'LOCATION': servers,
+#                 'OPTIONS': {
+#                     'username': username,
+#                     'password': password,
+#                 }
+#             }
+#         }
+#     return {'default': cache}
+
+
+# CACHES = get_cache()
+
+
 def get_cache():
-    environment_ready = all(
-        os.environ.get(f'MEMCACHIER_{key}', False)
-        for key in ['SERVERS', 'USERNAME', 'PASSWORD']
-    )
-    if not environment_ready:
-        cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
-    else:
-        servers = os.environ['MEMCACHIER_SERVERS']
-        username = os.environ['MEMCACHIER_USERNAME']
-        password = os.environ['MEMCACHIER_PASSWORD']
-        cache = {
-            'default': {
-                'BACKEND': 'django_bmemcached.memcached.BMemcached',
-                'TIMEOUT': None,
-                'LOCATION': servers,
-                'OPTIONS': {
-                    'username': username,
-                    'password': password,
-                }
-            }
+  import os
+  try:
+    servers = os.environ['MEMCACHIER_SERVERS']
+    username = os.environ['MEMCACHIER_USERNAME']
+    password = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+        'LOCATION': servers,
+        'OPTIONS': {
+          'binary': True,
+          'username': username,
+          'password': password,
+          'behaviors': {
+            # Enable faster IO
+            'no_block': True,
+            'tcp_nodelay': True,
+            # Keep connection alive
+            'tcp_keepalive': True,
+            # Timeout settings
+            'connect_timeout': 2000, # ms
+            'send_timeout': 750 * 1000, # us
+            'receive_timeout': 750 * 1000, # us
+            '_poll_timeout': 2000, # ms
+            # Better failover
+            'ketama': True,
+            'remove_failed': 1,
+            'retry_timeout': 2,
+            'dead_timeout': 30,
+          }
         }
-    return {'default': cache}
-
-
+      }
+    }
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
 CACHES = get_cache()
